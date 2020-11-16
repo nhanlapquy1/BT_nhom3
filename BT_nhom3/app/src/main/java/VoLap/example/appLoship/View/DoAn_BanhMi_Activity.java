da8121bafa82d6c292342bfea80250180b3ac3d2
Vo Lap <knvolap@gmail.com>
2020-11-16 20:36:49 +0700

    16/11
    90%

--- BT_nhom3/app/src/main/java/VoLap/example/appLoship/View/DoAn_BanhMi_Activity.java
+++ BT_nhom3/app/src/main/java/VoLap/example/appLoship/View/DoAn_BanhMi_Activity.java
@@ -3,6 +3,7 @@ package VoLap.example.appLoship.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.circleview.R;
 
 public class DoAn_BanhMi_Activity extends AppCompatActivity{
-ImageView img_back_giaodoan3;
+    ImageView image_back_giaodoan3, image_giohang;
+    Button btDatNgay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,33 @@ ImageView img_back_giaodoan3;
 
         overridePendingTransition(R.anim.trai_sang_phai,R.anim.phai_sang_trai);
 
-        img_back_giaodoan3 = (ImageView)findViewById(R.id.btn_back_menudoan);
-        img_back_giaodoan3.setOnClickListener(new View.OnClickListener() {
+        image_back_giaodoan3 = (ImageView)findViewById(R.id.btn_back_menudoan);
+        image_back_giaodoan3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent( DoAn_BanhMi_Activity.this,Menu_GiaoDoAn.class);
                 startActivity(intent);
             }
         });
+
+        image_giohang = (ImageView)findViewById(R.id.img_giohang);
+        image_giohang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent( DoAn_BanhMi_Activity.this,GioHangActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        btDatNgay=(Button)findViewById(R.id.btn_bm_DatNgay);
+        btDatNgay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3=new Intent(DoAn_BanhMi_Activity.this, GioHangActivity.class);
+                startActivity(intent3);
+            }
+        });
+
 
     }
 }
---
+++ BT_nhom3/app/src/main/java/VoLap/example/appLoship/View/GioHangActivity.java
@@ -0,0 +1,33 @@
+package VoLap.example.appLoship.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.circleview.R;
+
+public class GioHangActivity extends AppCompatActivity {
+    Button bt_giohang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gio_hang);
+
+        bt_giohang=(Button)findViewById(R.id.btn_giohang);
+        bt_giohang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Dis2=new Intent(GioHangActivity.this, GiaoDoAn_activity.class);
+                startActivity(Dis2);
+            }
+        });
+
+
+    }
+
+
+}
