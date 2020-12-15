258b15ab43cf9e8eba66306af26f77969e820067
knvolap <luongluongluong987@gmail.com>
2020-12-15 09:56:45 +0700

    lap 25.12

--- BT_nhom3/app/src/androidTest/java/VoLap/example/appLoship/ProfileUserActivityTest.groovy
+++
@@ -1,6 +0,0 @@
-package VoLap.example.appLoship
-
-import VoLap.example.appLoship.Model.Bottom_home
-
-class ProfileUserActivityTest extends Bottom_home {
-}
--- BT_nhom3/app/src/main/java/VoLap/example/appLoship/Model/Bottom_home.java
+++
@@ -1,4 +0,0 @@
-package VoLap.example.appLoship.Model;
-
-public class Bottom_home {
-}
